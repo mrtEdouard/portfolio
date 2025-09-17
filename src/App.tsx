0b0806cae3070ecada63b3d4b4cbf48c2ca2feb0
mrtEdouard <ed.mouret34530@gmail.com>
2025-09-17 17:06:36 +0200

    Initial commit: portfolio React

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// src/App.tsx
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <main id="projects">
+        {/* plus tard tu mets ici ta section Projets */}
+      </main>
+      
+    </>
+    
+  );
+}
+
+export default App;
